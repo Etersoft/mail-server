409690d08274e1932495679263653dc49a5e5769
Dmitry Nikulin <theowl@etersoft.ru>
2018-04-11 19:27:38 +0300

    Изменён формат генерации list-id

--- server/src/getListId.ts
+++ server/src/getListId.ts
@@ -2,5 +2,7 @@ import { Mailing } from './Mailing';
 
 
 export function getListId (config: any, mailing: Mailing): string {
-  return config.server.mail.listIdPrefix + mailing.id + '-' + new Date().toISOString();
+  const date = new Date();
+  const dateFormatted = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return config.server.mail.listIdPrefix + mailing.id + '_' + dateFormatted;
 }
