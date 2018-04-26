e84f1c7c767be34700ac632e32596e39f03b195c
Dmitry Nikulin <theowl@etersoft.ru>
2018-04-26 13:24:19 +0300

    Корректное формирование List-Id

--- server/src/getListId.ts
+++ server/src/getListId.ts
@@ -1,8 +1,9 @@
 import { Mailing } from './Mailing';
+import * as moment from 'moment';
 
 
 export function getListId (config: any, mailing: Mailing): string {
   const date = new Date();
-  const dateFormatted = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-  return config.server.mail.listIdPrefix + mailing.id + '_' + dateFormatted;
+  const dateFormatted = moment().format('YYYYMMDD');
+  return `<${dateFormatted}-${mailing.id}@${config.server.mail.listIdDomain}>`;
 }
---
+++ server/tests/getListId.ts
@@ -0,0 +1,41 @@
+// tslint:disable:no-shadowed-variable
+import { assert, use } from 'chai';
+import 'mocha';
+import { useFakeTimers } from 'sinon';
+import { getListId } from '../src/getListId';
+import { Mailing } from '../src/Mailing';
+
+
+describe('getListId', () => {
+  const fakeConfig = {
+    server: {
+      mail: {
+        listIdDomain: 'testPrefix'
+      }
+    }
+  };
+  const fakeMailing = { id: 1 };
+
+  it('should use angle brackets', () => {
+    const listId = getListId(fakeConfig, fakeMailing as Mailing);
+    assert.equal(listId[0], '<');
+    assert.equal(listId[listId.length - 1], '>');
+  });
+
+  it('should use current date', () => {
+    const now = new Date(2018, 3, 1);
+    const timers = useFakeTimers(now);
+    const listId = getListId(fakeConfig, fakeMailing as Mailing).replace(/^<|>$/g, '');
+    assert.equal(listId.split('-')[0], `20180401`);
+  });
+
+  it('should use mailing ID', () => {
+    const listId = getListId(fakeConfig, fakeMailing as Mailing).replace(/^<|>$/g, '');
+    assert.equal(listId.split('-')[1].split('@')[0], '1');
+  });
+
+  it('should use domain from config', () => {
+    const listId = getListId(fakeConfig, fakeMailing as Mailing).replace(/^<|>$/g, '');
+    assert.equal(listId.split('@')[1], fakeConfig.server.mail.listIdDomain);
+  });
+});
